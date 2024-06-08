af53ef82b5eb9ef461c7f3055f09766a06527d0a
NicoleRau <nicole.rau@stud.fhgr.ch>
2024-06-08 14:00:56 +0200

    Kontaktformular
    Header eingefügt
    am Formular herumprobiert

--- kontaktformular.js
+++ kontaktformular.js
@@ -20,3 +20,35 @@
     // Formular zurücksetzen
     document.getElementById('contactForm').reset();
 }); */
+
+document.addEventListener("DOMContentLoaded", function() {
+    const iconOptions = document.querySelectorAll(".icon-option input");
+
+    iconOptions.forEach(option => {
+        option.addEventListener("change", function() {
+            // Alle Bilder zurücksetzen
+            iconOptions.forEach(opt => {
+                const img = opt.nextElementSibling.querySelector("img");
+                img.src = img.getAttribute("src");
+            });
+
+            // Ausgewähltes Bild ändern
+            const selectedImg = this.nextElementSibling.querySelector("img");
+            selectedImg.src = selectedImg.getAttribute("data-selected");
+        });
+
+        // Hover-Effekte hinzufügen
+        const label = option.nextElementSibling;
+        const img = label.querySelector("img");
+        label.addEventListener("mouseover", function() {
+            img.src = img.getAttribute("data-hover");
+        });
+        label.addEventListener("mouseout", function() {
+            if (!option.checked) {
+                img.src = img.getAttribute("src");
+            } else {
+                img.src = img.getAttribute("data-selected");
+            }
+        });
+    });
+});
